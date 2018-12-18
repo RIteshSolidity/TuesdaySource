2aaf6c9987cbbd2af495cb3d78df0df14ead94b4
Ritesh Modi <callritz@hotmail.com>
2018-12-18 09:40:38 +0000

    initial commit

---
+++ TopicSB/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.Azure.ServiceBus.Core;
+using Newtonsoft.Json;
+
+namespace TopicSB
+{
+    class Program
+    {
+        private static ServiceBusConnection connection;
+        static void Main(string[] args)
+        {
+            ServiceBusConnectionStringBuilder builder = new ServiceBusConnectionStringBuilder("Endpoint=sb://tuesdaysb.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=4fDfjFmsgSyqw7y9802cPNv2hLabMxm263upNBxRw7k=");
+
+            connection = new ServiceBusConnection(builder);
+            ConnectToServiceBus().GetAwaiter().GetResult();
+            Console.ReadLine();
+        }
+
+        private async static Task ConnectToServiceBus()
+        {
+            TopicClient client = new TopicClient(connection, "topicsb", RetryPolicy.Default);
+            //Guid Productid = Guid.NewGuid();
+
+
+           // var jsonContent = JsonConvert.SerializeObject(p1);
+
+            Message msg = new Message(Encoding.ASCII.GetBytes("Test Message"));
+
+            await client.SendAsync(msg);
+
+        }
+    }
+}
---
+++ subscriber1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.Azure.ServiceBus.Core;
+using Newtonsoft.Json;
+
+namespace subscriber1
+{
+    class Program
+    {
+        private static ServiceBusConnection connection;
+        private static SubscriptionClient client;
+        static void Main(string[] args)
+        {
+            ServiceBusConnectionStringBuilder builder = new ServiceBusConnectionStringBuilder("Endpoint=sb://tuesdaysb.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=4fDfjFmsgSyqw7y9802cPNv2hLabMxm263upNBxRw7k=");
+
+            connection = new ServiceBusConnection(builder);
+            ConnectToServiceBus().GetAwaiter().GetResult();
+            Console.ReadLine();
+        }
+
+        private async static Task ConnectToServiceBus()
+        {
+             client = new SubscriptionClient(connection, "topicsb", "topicsSubscription", ReceiveMode.PeekLock, RetryPolicy.Default);
+            //Guid Productid = Guid.NewGuid();
+
+            client.RegisterMessageHandler(happypath, unhappypath);
+            // var jsonContent = JsonConvert.SerializeObject(p1);
+            await Task.CompletedTask;
+
+        }
+
+        private static Task unhappypath(ExceptionReceivedEventArgs arg)
+        {
+            
+            throw new NotImplementedException();
+        }
+
+        private async static Task happypath(Message arg1, CancellationToken arg2)
+        {
+            string incomingMessage = Encoding.ASCII.GetString(arg1.Body);
+  
+            Console.WriteLine(incomingMessage);
+            
+            await Task.CompletedTask;
+        }
+    }
+}
